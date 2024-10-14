338135f8837110ada438fc6024fda02509dfd9ac
Cahit Karahan <trincitasdad@gmail.com>
2024-10-14 15:09:25 +0300

    No need to wait for on-chain data for days passed

--- WEB3/assets/script.js
+++ WEB3/assets/script.js
@@ -105,10 +105,11 @@ function risyData() {
                 return parseFloat(value) || 0;
             },
             async init() {
+                this.days = this.daysPassed.toFixed(0).toString();
+
                 await connector.calculateUniswapV2PriceAsNum("0xb908228A001CB177ac785659505EBCa1d9947EE8","0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359").then(price => {
                     this.currentPrice = price.toFixed(12).toString();
                     this.dailyReturn = (this.currentDailyReturn * 100).toFixed(2).toString();
-                    this.days = this.daysPassed.toFixed(0).toString();
                 });
                 
                 setInterval(async () => {
@@ -340,7 +341,6 @@ function risyData() {
                 this.onChainData.totalSupplyUSD = (parseFloat(this.onChainData.totalSupply) * parseFloat(this.onChainData.currentPrice)).toFixed(2);
                 this.onChainData.maxBalanceUSD = (parseFloat(this.onChainData.maxBalance) * parseFloat(this.onChainData.currentPrice)).toFixed(2);
                 this.profitCalculator.capital = this.onChainData.maxBalanceUSD;
-                this.profitCalculator.days = this.profitCalculator.daysPassed.toFixed(0).toString();
             } catch (error) {
                 // Try again after 5 seconds
                 console.error('Error fetching on-chain data:', error);
